2d6577bbf5c2a9d9d1aa6442931098b432fe6d48
ONKAR SAWANT <169951107+Buildwithtiger@users.noreply.github.com>
2026-07-15 02:33:46 +0530

    Add files via upload

---
+++ lec13array2.java
@@ -0,0 +1,8 @@
+public class lec13array2 {
+    public static void main(String[] args) {
+        int arr[]={11,22,33,44,55,66,77,88,99,00,11};
+        for (int val:arr){
+            System.out.println(val);
+        }
+    }
+}
