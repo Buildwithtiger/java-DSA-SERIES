cedd5ed0df6944f089b23c49f09e26f19fa7a1f9
ONKAR SAWANT <169951107+Buildwithtiger@users.noreply.github.com>
2026-07-15 02:33:56 +0530

    Add files via upload

---
+++ LEC13array.java
@@ -0,0 +1,17 @@
+public class LEC13array {
+    public static void main(String[] args) {
+        int arr[];
+        arr=new int[7];
+        int brr[]={11,22,33,44};
+        System.out.println(arr);
+        int n=brr.length;
+        for(int index=0;index<=n-1;index++){
+            System.out.println(brr[index]);
+        }
+//        System.out.println(brr[1]);
+//        System.out.println(brr[2]);
+//        System.out.println(brr[3]);
+//
+
+    }
+}
