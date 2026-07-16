0ad5350c7b28aa67b90e281f0294f58e8e509c8d
ONKAR SAWANT <169951107+Buildwithtiger@users.noreply.github.com>
2026-07-16 22:13:57 +0530

    Add files via upload

---
+++ lec12m4.java
@@ -0,0 +1,11 @@
+public class lec12m4 {
+    static void table(int c){
+        for (int i=1;i<=10;i++){
+            System.out.println(i*c);
+        }
+    }
+
+    public static void main(String[] args) {
+        table(22);
+    }
+}
