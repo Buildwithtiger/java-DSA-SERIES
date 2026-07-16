4feae8a1ddd28dab255439ea7660a384d1ffab43
ONKAR SAWANT <169951107+Buildwithtiger@users.noreply.github.com>
2026-07-16 22:13:45 +0530

    Add files via upload

---
+++ lec12m5.java
@@ -0,0 +1,13 @@
+public class lec12m5 {
+
+    static void multiplication(int a,int b){
+        System.out.println(a*b);
+        return;
+    }
+
+    public static void main(String[] args) {
+
+        multiplication(22,33);
+    }
+}
+        
