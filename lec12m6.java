6d39e595f05a61ba7ac9660da17484a5a93c5d39
ONKAR SAWANT <169951107+Buildwithtiger@users.noreply.github.com>
2026-07-16 22:13:20 +0530

    Add files via upload

---
+++ lec12m6.java
@@ -0,0 +1,10 @@
+public class lec12m6 {
+    static int add(int a,int b){
+       int sum=a+b;
+       return sum;
+    }
+    public static void main(String[] args) {
+       int resulr= add(22,44);
+        System.out.println( resulr);
+    }
+}
